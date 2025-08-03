ac9eb41fd0c80aadb8cd189b47bc5bb29c7c2a83
Greeshma <chinthagreeshma13@gmail.com>
2025-08-03 12:14:04 +0530

    modified logic

--- Hrs_vs_Scores.py
+++ Hrs_vs_Scores.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt        # For plotting graphs and visualizations
 from sklearn.model_selection import train_test_split   # For splitting data into training and testing
 from sklearn.linear_model import LinearRegression      # For creating a simple linear regression model
 from sklearn import metrics            # For checking model accuracy (errors, score, etc.)
-print('starting pgm')
+print('starting program')
 # Read CSV and remove unwanted columns
 data = pd.read_csv('F:/Python/Hrs_vs_Scores.csv')
 
